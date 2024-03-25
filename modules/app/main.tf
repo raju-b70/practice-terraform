d9097a5acb7a9733261a3016e985a3ce5aad24ba
rajendra <rajendrammarboyana@gmail.com>
2024-03-25 08:11:25 +0000

    project setup v1

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -25,7 +25,7 @@ resource "null_resource" "ansible" {
 
 resource "aws_route53_record" "record" {
   name    = "$(var.component)-$(var.env)"
-  type    = "a"
+  type    = "A"
   zone_id = var.zone_id
   records = [aws_instance.instance.private_ip]
   ttl     = 30
