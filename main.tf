a326fb012641e00bf6935752b0de25c032d553f5
rajendra <rajendrammarboyana@gmail.com>
2024-03-25 10:35:07 +0000

    project setup v1

--- main.tf
+++ main.tf
@@ -7,9 +7,24 @@ module "frontend" {
   env = var.env
   zone_id = var.zone_id
 }
-#module "backend" {
- # source = ""
-#}
-#module "mysql" {
-#  source = ""
-#}
+
+module "backend" {
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component = "backend"
+  ssh_user = var.ssh_user
+  ssh_pass = var.ssh_pass
+  env = var.env
+  zone_id = var.zone_id
+}
+
+module "mysql" {
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component = "mysql"
+  ssh_user = var.ssh_user
+  ssh_pass = var.ssh_pass
+  env = var.env
+  zone_id = var.zone_id
+}
+
