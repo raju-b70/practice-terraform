c91d38c06de571fbc6d0d477d3d69258a4fdfc46
rajendra <rajendrammarboyana@gmail.com>
2024-03-25 08:08:43 +0000

    project setup v1

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "ami" {
+resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
